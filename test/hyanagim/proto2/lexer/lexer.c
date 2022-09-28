5672614b1f947a3652d43531b0864e64d0ef9f77
Hayato Yanagimura <hyanagim@c1r31s7.42tokyo.jp>
2022-09-28 19:46:13 +0900

    a little

--- test/hyanagim/proto2/lexer/lexer.c
+++ test/hyanagim/proto2/lexer/lexer.c
@@ -6,15 +6,12 @@
 /*   By: hyanagim <hyanagim@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 21:53:35 by hyanagim          #+#    #+#             */
-/*   Updated: 2022/09/27 12:36:26 by hyanagim         ###   ########.fr       */
+/*   Updated: 2022/09/28 19:43:31 by hyanagim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../../libft/libft.h"
 #include "../main/minishell.h"
-
-
-
 
 static int	match_quote(char *str, size_t *i, char c)
 {
