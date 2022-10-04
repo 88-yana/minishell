afdabfc4f73a7632070ae2c1e13edad919c17bab
Hayato Yanagimura <hyanagim@c1r31s7.42tokyo.jp>
2022-10-04 16:59:33 +0900

    before_malloc_safe

--- test/trial/lexer/divide_quote/bool_re.c
+++ test/trial/lexer/divide_quote/bool_re.c
@@ -6,7 +6,7 @@
 /*   By: hyanagim <hyanagim@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 17:11:40 by hyanagim          #+#    #+#             */
-/*   Updated: 2022/10/04 13:21:42 by hyanagim         ###   ########.fr       */
+/*   Updated: 2022/10/04 14:00:27 by hyanagim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,10 @@ bool	is_separator_re(char c, char d)
 		return (true);
 	if ((c == '>' && d == '>') || d == '\0')
 		return (true);
+	if ((c == '|' && d == '|') || d == '\0')
+		return (true);
+	if ((c == '&' && d == '&') || d == '\0')
+		return (true);
 	return (false);
 }
 
@@ -26,6 +30,10 @@ bool	is_ltltgtgt(char c, char d)
 	if (c == '<' && d == '<')
 		return (true);
 	if (c == '>' && d == '>')
+		return (true);
+	if (c == '&' && d == '&')
+		return (true);
+	if (c == '|' && d == '|')
 		return (true);
 	return (false);
 }
