f7e6b37ff39de54d1709ac39e90440be0662df3e
Hayato Yanagimura <hyanagim@c1r24s2.42tokyo.jp>
2022-12-31 18:32:51 +0900

    all

--- srcs/mandatory/execution/delete_quote.c
+++ srcs/mandatory/execution/delete_quote.c
@@ -6,21 +6,21 @@
 /*   By: hyanagim <hyanagim@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 15:36:18 by hyanagim          #+#    #+#             */
-/*   Updated: 2022/12/31 17:58:42 by hyanagim         ###   ########.fr       */
+/*   Updated: 2022/12/31 18:31:44 by hyanagim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
 
-// void	del(char *str, char quote, size_t i)
-// {
-// 	ft_strlcpy(&str[i], &str[i + 1], ft_strlen(&str[i]));
-// 	while (str[i] != '\0' && str[i] != quote)
-// 		i++;
-// 	if (str[i] == '\0')
-// 		return ;
-// 	ft_strlcpy(&str[i], &str[i + 1], ft_strlen(&str[i]));
-// }
+void	del(char *str, char quote, size_t i)
+{
+	ft_strlcpy(&str[i], &str[i + 1], ft_strlen(&str[i]));
+	while (str[i] != '\0' && str[i] != quote)
+		i++;
+	if (str[i] == '\0')
+		return ;
+	ft_strlcpy(&str[i], &str[i + 1], ft_strlen(&str[i]));
+}
 
 void	delete_quote(char *str)
 {
@@ -29,24 +29,9 @@ void	delete_quote(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] == DOUBLEQ)
+		if (str[i] == DOUBLEQ || str[i] == SINGLEQ)
 		{
-			ft_strlcpy(&str[i], &str[i + 1], ft_strlen(&str[i]));
-			while (str[i] != '\0' && str[i] != DOUBLEQ)
-				i++;
-			if (str[i] == '\0')
-				return ;
-			ft_strlcpy(&str[i], &str[i + 1], ft_strlen(&str[i]));
-			continue ;
-		}
-		if (str[i] == SINGLEQ)
-		{
-			ft_strlcpy(&str[i], &str[i + 1], ft_strlen(&str[i]));
-			while (str[i] != '\0' && str[i] != SINGLEQ)
-				i++;
-			if (str[i] == '\0')
-				break ;
-			ft_strlcpy(&str[i], &str[i + 1], ft_strlen(&str[i]));
+			del(str, str[i], i);
 			continue ;
 		}
 		i++;
