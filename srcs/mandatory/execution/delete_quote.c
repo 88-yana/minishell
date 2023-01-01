40f59c8c0ef336ab448e3b5552b684face4ca562
Hayato Yanagimura <hyanagim@c1r24s2.42tokyo.jp>
2023-01-01 14:42:53 +0900

    quote

--- srcs/mandatory/execution/delete_quote.c
+++ srcs/mandatory/execution/delete_quote.c
@@ -6,32 +6,29 @@
 /*   By: hyanagim <hyanagim@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 15:36:18 by hyanagim          #+#    #+#             */
-/*   Updated: 2022/12/31 18:31:44 by hyanagim         ###   ########.fr       */
+/*   Updated: 2023/01/01 14:42:37 by hyanagim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
 
-void	del(char *str, char quote, size_t i)
-{
-	ft_strlcpy(&str[i], &str[i + 1], ft_strlen(&str[i]));
-	while (str[i] != '\0' && str[i] != quote)
-		i++;
-	if (str[i] == '\0')
-		return ;
-	ft_strlcpy(&str[i], &str[i + 1], ft_strlen(&str[i]));
-}
-
 void	delete_quote(char *str)
 {
 	size_t	i;
+	char	quote;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
 		if (str[i] == DOUBLEQ || str[i] == SINGLEQ)
 		{
-			del(str, str[i], i);
+			quote = str[i];
+			ft_strlcpy(&str[i], &str[i + 1], ft_strlen(&str[i]));
+			while (str[i] != '\0' && str[i] != quote)
+				i++;
+			if (str[i] == '\0')
+				return ;
+			ft_strlcpy(&str[i], &str[i + 1], ft_strlen(&str[i]));
 			continue ;
 		}
 		i++;
