ca9c7574164952f0c0140e4c1f8e6bbde91758d2
Hayato Yanagimura <hyanagim@c1r24s2.42tokyo.jp>
2023-01-03 18:48:43 +0900

    pipe

--- srcs/bonus/reader/check_lex_bonus.c
+++ srcs/bonus/reader/check_lex_bonus.c
@@ -1,22 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   check_lex.c                                        :+:      :+:    :+:   */
+/*   check_lex_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hyanagim <hyanagim@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/01 19:10:46 by hyanagim          #+#    #+#             */
-/*   Updated: 2023/01/01 19:14:14 by hyanagim         ###   ########.fr       */
+/*   Updated: 2023/01/03 18:46:39 by hyanagim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "reader_bonus.h"
 
-bool	check_lex(t_str	*current)
+bool	check_lex(t_str	*head)
 {
+	t_str	*current;
+
+	current = head;
 	while (current && current->next)
 	{
 		if (current->type == BRA && current->next->type == CKET)
+			return (false);
+		current = current->next;
+	}
+	current = head;
+	while (current)
+	{
+		if (current->type == PIPE && current->next == NULL)
+			return (false);
+		if (current->type == PIPE && current->prev == NULL)
 			return (false);
 		current = current->next;
 	}
